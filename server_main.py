fbb01ad9c231a29568cbaff30d007fc5c3b8bfbe
nts30 <113692559+nts30@users.noreply.github.com>
2022-10-22 16:46:03 +0300

    Add files via upload

--- server_main.py
+++ server_main.py
@@ -3,27 +3,31 @@ import threading
 
 
 class Server:
-    def __init__(self, host: str, port: str):
+    def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
         self.pool = threading.Thread(target=self._server)
         self.pool.start()
 
     def _server(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind((self.host, self.port))
-        clients_address = []
-        print('Start server!')
+        client = []
+        print('Start Server')
 
         while True:
             data, address = sock.recvfrom(1024)
-            if address not in clients_address:
-                clients_address.append(address)
-                print(f'{clients_address[-1]} join.')
-            for client in clients_address:
-                if client == address:
+            print(address[0], address[1])
+            if address not in client:
+                client.append(address)
+                print(client)
+            for clients in client:
+                if clients == address:
                     continue
-                sock.sendto(data, client)
+                sock.sendto(data, clients)
+                print(data, clients)
 
 
-server = Server(host='127.0.0.1', port=9090)
+host = '127.0.0.1'
+port = 9090
+server = Server(host=host, port=port)
